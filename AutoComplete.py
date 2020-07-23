5c6276ec67ffb560f40d3176401ad2d1763d81df
zimerman <zr@sem-wolf.org>
2020-07-23 04:32:00 +0300

    add get functions to attribute in class

--- AutoComplete.py
+++ AutoComplete.py
@@ -10,3 +10,15 @@ class Autocomplete:
         print(self.__source_text)
         print(self.__offset)
         print(self.__score)
+
+    def get_completed_sentence(self):
+        return self.__completed_sentence
+
+    def get_source_text(self):
+        return self.__source_text
+
+    def get_offset(self):
+        return self.__offset
+
+    def get_score(self):
+        return self.__score
