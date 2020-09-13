ed09f3f923ba2f5569f63a3ee098ec90e8ac6975
zimerman <zr@sem-wolf.org>
2020-09-14 01:44:20 +0300

    dividing to function

--- AutoComplete.py
+++ AutoComplete.py
@@ -5,29 +5,29 @@ from AutoCompleteData import Autocomplete
 
 
 def is_exist_completion(list_dict, complete_string):
-    for item in list_dict:
-        if listString[item["index"]]["sentence"] == complete_string:
+    for dict_complate in list_dict:
+        if listString[dict_complate["index"]]["sentence"] == complete_string:
             return True
     return False
 
 
 def change(string, count, list_dict):
     index = len(string)
-    for lenghString, word in enumerate(string[::-1], 1):
-        index = index - 1
-        for x in range(ord('a'), ord('z') + 1):
-            sub_string = string[:-lenghString] + chr(x) + string[len(string) - lenghString + 1:]
+    for index_string, word in enumerate(string[::-1], 1):
+        index = index-1
+        for x in range(ord('a'), ord('z')+1):
+            sub_string = string[:-index_string]+chr(x)+string[len(string)-index_string+1:]
             if dict_data.get(sub_string):
-                if chr(x) != string[-lenghString]:
+                if chr(x) != string[-index_string]:
                     score = get_score(index)
                     add_completion(sub_string, count, 2*len(string)-score, list_dict)
 
 
-def add(string, count,list_dict):
+def add(string, count, list_dict):
     index = len(string)
-    for lenghString, word in enumerate(string[::-1],1):
-        index = index - 1
-        for x in range(ord('a'), ord('z') + 1):
+    for word in string[::-1]:
+        index = index-1
+        for x in range(ord('a'), ord('z')+1):
             sub_string = string.replace(word, chr(x)+word)
             if dict_data.get(sub_string):
                 score = get_score(index)
@@ -36,22 +36,22 @@ def add(string, count,list_dict):
 
 def delete(string, count, list_dict):
     index = len(string)
-    for lenghString, word in enumerate(string[::-1],1):
+    for index_string, word in enumerate(string[::-1], 1):
         index = index-1
-        sub_string = string[:-lenghString] + string[len(string) - lenghString+1:]
+        sub_string = string[:-index_string] + string[len(string)-index_string+1:]
         if dict_data.get(sub_string):
             score = get_score(index)
-            add_completion(sub_string, 2 * len(string)-2 * count, score, list_dict)
+            add_completion(sub_string, 2*len(string)-2*count, score, list_dict)
 
 
-def changeInput(string, count, list_dict):
+def fix_input(string, count, list_dict):
     change(string, count, list_dict)
     delete(string, count, list_dict)
     add(string, count, list_dict)
     list_dict = sorted(list_dict, key=lambda k: k["score"], reverse=True)
 
 
-def autocompleteData(string):
+def get_best_5completions(string):
     list1 = []
     list_dict = []
     count = 0
@@ -61,9 +61,9 @@ def autocompleteData(string):
                 list_dict.append({"index": index, "score": 2*len(string)})
                 count = count+1
         if count < 5:
-            changeInput(string,count,list_dict)
+            fix_input(string, count, list_dict)
     else:
-        changeInput(string, count, list_dict)
+        fix_input(string, count, list_dict)
     i = 0
     for item in list_dict:
         if i < 5:
@@ -77,13 +77,11 @@ def add_completion(string, count, score, list_dict):
     for item in dict_data[string]:
         if not is_exist_completion(list_dict, listString[item]["sentence"]) and count < 5:
             list_dict.append({"index": item, "score": score})
-            count = count + 1
+            count = count+1
 
 
 def get_score(index):
     if index < 5:
-        score = 5 - index
+        return 5-index
     else:
-        score = 1
-    return score
-
+        return 1
--- AutoCompleteData.py
+++ AutoCompleteData.py
@@ -1,6 +1,6 @@
 class Autocomplete:
-    def __init__(self,completed_sentence,source_text,offset,score):
-        self.__completed_sentence =completed_sentence
+    def __init__(self, completed_sentence, source_text, offset, score):
+        self.__completed_sentence = completed_sentence
         self.__source_text = source_text
         self.__offset = offset
         self.__score = score
