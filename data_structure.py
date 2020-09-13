555a1513af1b198c68d8584c3468c719fbe300f6
zimerman <zr@sem-wolf.org>
2020-09-14 01:45:30 +0300

    read from file

--- data_structure.py
+++ data_structure.py
@@ -1,37 +1,40 @@
-import os
 import re
+import os
+
 listString = []
+dict_data = {}
 dictFile = {}
-sub_string_dict = {}
-
-
-def read_from_file():
-    for dirpath, dirnames, files in os.walk('python-3.8.4-docs-text', topdown=True):
-        for ind, file_name in enumerate(files, 1):
-            dictFile[ind] = file_name
-            init_list_dict(dirpath+"\\"+file_name, ind)
-
-
-def init_list_dict(path, ind):
-    with open(path, encoding='UTF-8') as the_file:
-        temp_list_string = the_file.read().split("\n")
-    for index_line, line in enumerate(temp_list_string, 1):
-        listString.append({"sentence": line, "source": ind, "offset": index_line})
-
-
-def init_data():
-    for ind_sent in range(len(listString)):
-        for index in range(len(listString[ind_sent]["sentence"])):
-            for index2 in range(index+1, len(listString[ind_sent]["sentence"])+1):
-                sub_string_dict.setdefault((ignore_character(listString[ind_sent]["sentence"])[index:index2]), set()).add(ind_sent)
-                sub_string_dict.setdefault((ignore_character(listString[ind_sent]["sentence"])[index2:index]), set()).add(ind_sent)
-    for sentence in sub_string_dict.keys():
-        sub_string_dict[sentence] = list(sub_string_dict[sentence])
-        sub_string_dict[sentence] = sorted(sub_string_dict[sentence], key=lambda k: listString[k]["sentence"])
 
 
 def ignore_character(string):
     return re.sub('[^a-z0-9' ']+', '', string.lower())
 
 
+def read_from_file():
+    for root, dirs, files in os.walk('data', topdown=True):
+        for line_index, file in enumerate(files, 1):
+            dictFile[line_index] = root+'\\'+file
+            init_data(root+'\\'+file, line_index)
+
+
+def init_data(file, index_source):
+    with open(file, encoding='UTF-8') as the_file:
+        templist_string = the_file.read().split("\n")
+    for i, string in enumerate(templist_string, 1):
+        listString.append({"sentence": string, "source": index_source, "offset": i})
+
+
+def init_substrings():
+    for j in range(len(listString)):
+        for index1 in range(len(listString[j]["sentence"]) + 1):
+            for index2 in range(index1+1, len(listString[j]["sentence"])+1):
+                dict_data.setdefault(ignore_character(listString[j]["sentence"][index1:index2]), set()).add(j)
+                dict_data.setdefault(ignore_character(listString[j]["sentence"][index2:index1]), set()).add(j)
+    for sentence in dict_data.keys():
+        dict_data[sentence] = list(dict_data[sentence])
+        dict_data[sentence] = sorted(dict_data[sentence], key=lambda k: listString[k]["sentence"])
+
+
+read_from_file()
+init_substrings()
 
